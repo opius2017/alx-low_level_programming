629cae5622934bfe10a8c7067d846b2e9c9c5ea7
Owolabi Pius <opius2007@gmail.com>
2022-12-08 15:53:40 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
-/**                                          
- *
- * main - Entry point
- *
- * Return: Always 0 (Success)
- *
- */                                                     
+/**
+*
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*
+*/
 
 int main(void)
 {
-	char str[]= "\"Programming is like building a multilingual puzzle";
-        puts(str);
+	char str[] = "\"Programming is like building a multilingual puzzle";
+	puts(str);
 	return (0);
-}      
+}

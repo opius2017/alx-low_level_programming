b0728613aed695d2da80bac3f81990441e172163
Owolabi Pius <opius2007@gmail.com>
2022-12-13 08:15:31 -0800

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Print the word "_putchar"
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int i;
+	char p[] = "_putchar";
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(p[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}

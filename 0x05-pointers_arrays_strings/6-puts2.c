076d5ad18252f7dee7826a512a75eac9da160498
Owolabi Pius <opius2007@gmail.com>
2022-12-19 19:29:21 -0800

    Task 7

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
  * puts2 - prints every other character of a string
  * @str: char to check
  * Return: 0 is success
  */
+
 void puts2(char *str)
 {
 	int string;
@@ -11,5 +13,6 @@ void puts2(char *str)
 	for (string = 0; str[string] != '\0'; string++)
 	if (string % 2 == 0)
 		_putchar(str[string]);
+
 	_putchar('\n');
 }
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -9,11 +9,10 @@ void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
-/*void print_array(int *a, int n);*/
-/*void print_array(int *a, int n);*/
-/*char *_strcpy(char *dest, char *src);*/
-/*int _atoi(char *s);*/
-/*int _putchar(char c);*/
-
+void print_array(int *a, int n);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+int _putchar(char c);
 
 #endif 

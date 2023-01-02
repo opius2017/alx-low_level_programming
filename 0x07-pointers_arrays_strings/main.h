21dcd87afdd5c8a818ad183ec55a3ca01ce95340
Owolabi Pius <opius2007@gmail.com>
2023-01-02 15:35:32 -0800

    A function that copies memory area

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,11 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stddef.h>
 
-#include <stdio.h>
-#include <stdlib.h>
 int _putchar(char c);
-char *_memset(char *s, char b, unsigned int n);
+void *_memset(void *s, int c, size_t n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
@@ -14,4 +13,5 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
-#endif/*MAIN_H*/
+
+#endif

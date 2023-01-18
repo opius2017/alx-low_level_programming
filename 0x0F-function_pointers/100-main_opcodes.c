c19317abd9d1d2212539dd91258154b7fcf0872f
Owolabi Pius <opius2007@gmail.com>
2023-01-18 19:39:09 +0100

    A program that prints the opcodes of its own main function

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -33,9 +33,9 @@ int main(int argc, char *argv[])
 	for (i = 0; i < bytes; i++)
 		{
 			if (i == bytes - 1)
-		{
-			printf("%02hhx\n", arr[i]);
-			break;
+			{
+				printf("%02hhx\n", arr[i]);
+				break;
 			}
 			printf("%02hhx ", arr[i]);
 		}

1e269f063a07d7c408494ff812ba1cfcf209c875
Owolabi Pius <opius2007@gmail.com>
2023-01-26 21:02:36 +0100

    A function that adds a new node at the beginning of a list_t list.

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,45 +1,46 @@
 #include "lists.h"
-#include <stdlib.h>
-#include <string.h>
-/**
- * _strlen - finds the length of a string
- * @str: string to find the length of
- *
- * Return: length of string
- */
-unsigned int _strlen(char *str)
-{
-	unsigned int i;
-
-	for (i = 0; str[i]; i++)
-		;
-	return (i);
-}
 
 /**
- * add_node - adds a node to the beginning of a linked list
- * @head: double pointer to a lined list
- * @str: string to add to the new node
+ * add_node - adds a node at the beginning of the linked list
  *
- * Return: pointer to the new node
+ * @head: pointer to the first node of the list
+ * @str: string to add to new node in the list
+ *
+ * Return: NULL if it fails / starting addr of the list
  */
+
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new;
+	list_t *new_node;
+	size_t str_len = 0;
 
+	/*set strig length to 0 if its NULL*/
 	if (str == NULL)
+		str_len = 0;
+
+	/*count length of string*/
+	while (str[str_len] != '\0')
+		str_len++;
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
 		return (NULL);
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
-		return (NULL);
-	new->str = strdup(str);
-	if (new->str == NULL)
-	{
-		free(new);
-		return (NULL);
-	}
-	new->len = _strlen(new->str);
-	new->next = *head;
-	*head = new;
-	return (new);
+
+	/**
+	 * initialize the next addr of new_node
+	 * to NULL if head is NULL, if it's not
+	 * move the first node to the addr of new_node
+	 * ->next hence adding new_node at the beginning
+	 *  of the list
+	 */
+	if (*head == NULL)
+		new_node->next = NULL;
+	else
+		new_node->next = *head;
+
+	new_node->str = strdup(str);
+	new_node->len = str_len;
+	*head = new_node;
+
+	return (*head);
 }

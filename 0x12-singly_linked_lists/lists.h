05fc942cc4873d347c0014c41c58d53d6b6c5637
Owolabi Pius <opius2007@gmail.com>
2023-01-26 22:01:14 +0100

    Singly list file update

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,9 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef LIST_H
+#define LIST_H
 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 /**
  * struct list_s - singly linked list
@@ -17,6 +20,7 @@ typedef struct list_s
 	unsigned int len;
 	struct list_s *next;
 } list_t;
+
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);

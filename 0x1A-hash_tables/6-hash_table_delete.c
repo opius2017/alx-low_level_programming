e1c2dffc8c3d6bbee20aa48cbe269e3b5b0fd7a3
Owolabi Pius <opius2007@gmail.com>
2023-04-06 22:27:33 +0100

    0x1A. C - Hash tables

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+/**
+ * hash_table_delete - Deletes a hash table
+ * @ht: The hash table
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *node, *temp;
+	unsigned long int i;
+
+	if (!ht)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node)
+		{
+			temp = node->next;
+			free(node->key);
+			free(node->value);
+			free(node);
+			node = temp;
+		}
+	}
+
+	free(ht->array);
+	free(ht);
+}

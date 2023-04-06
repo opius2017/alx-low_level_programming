a396e013291c1d7d27674a53d60a8dd3144fed01
Owolabi Pius <opius2007@gmail.com>
2023-04-06 20:02:01 +0100

    0x1A. C - Hash tables

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,49 +12,47 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-        unsigned long int index;
-        hash_node_t *new_node, *current_node;
+	unsigned long int index;
+	hash_node_t *new_node, *current_node;
 
-        if (ht == NULL || key == NULL || *key == '\0')
-                return (0);
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (0);
 
-        index = key_index((const unsigned char *)key, ht->size);
-        current_node = ht->array[index];
+	index = key_index((const unsigned char *)key, ht->size);
+	current_node = ht->array[index];
 
-        while (current_node)
-        {
-                if (strcmp(current_node->key, key) == 0)
-                {
-                        free(current_node->value);
-                        current_node->value = strdup(value);
-                        if (current_node->value == NULL)
-                                return (0);
-                        return (1);
-                }
-                current_node = current_node->next;
-        }
+	while (current_node)
+	{
+		if (strcmp(current_node->key, key) == 0)
+		{
+			free(current_node->value);
+			current_node->value = strdup(value);
+			if (current_node->value == NULL)
+				return (0);
+			return (1);
+		}
+		current_node = current_node->next;
+	}
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
 
-        new_node = malloc(sizeof(hash_node_t));
-        if (new_node == NULL)
-                return (0);
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (0);
+	}
 
-        new_node->key = strdup(key);
-        if (new_node->key == NULL)
-        {
-                free(new_node);
-                return (0);
-        }
+	new_node->value = strdup(value);
+	if (new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (0);
+	}
 
-        new_node->value = strdup(value);
-        if (new_node->value == NULL)
-        {
-                free(new_node->key);
-                free(new_node);
-                return (0);
-        }
-
-        new_node->next = ht->array[index];
-        ht->array[index] = new_node;
-
-        return (1);
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+	return (1);
 }

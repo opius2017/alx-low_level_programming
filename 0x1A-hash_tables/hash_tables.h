cf9ad7773211f3be7e5a998befbdc14ed404278f
Owolabi Pius <opius2007@gmail.com>
2023-04-06 19:42:48 +0100

    0x1A. C - Hash tables

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -39,5 +39,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+
 
 #endif /* HASH_TABLES_H */

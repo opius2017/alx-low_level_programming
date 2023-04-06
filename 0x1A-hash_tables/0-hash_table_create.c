0d32312a6d55515e1d938cca3cbdfdd0561ccb70
Owolabi Pius <opius2007@gmail.com>
2023-04-06 23:19:35 +0100

    0x1A. C - Hash tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -3,8 +3,10 @@
 /**
  * hash_table_create - creates a hash table
  * @size: the size of the array to be created
+ *
  * Return: a pointer to the newly created hash table, or NULL on failure
  */
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table_created;
@@ -27,5 +29,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 	{
 		hash_table_created->array[i] = NULL;
 	}
+
 	return (hash_table_created);
 }

0ef81e7548e1f24055f8be4c3c0b939d3b3d6191
Owolabi Pius <opius2007@gmail.com>
2023-04-06 21:17:59 +0100

    0x1A. C - Hash tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,6 +1,5 @@
 #include "hash_tables.h"
 #include <stdlib.h>
-
 /**
  * hash_table_create - creates a hash table
  * @size: the size of the array to be created
@@ -8,37 +7,35 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *hash_table = NULL;
-    unsigned long int i;
+	hash_table_t *hash_table = NULL;
+	unsigned long int i;
 
-    if (size == 0)
-    {
-        return (NULL);
-    }
+	if (size == 0)
+	{
+		return (NULL);
+	}
 
-    /* Allocate memory for hash table structure */
-    hash_table = malloc(sizeof(hash_table_t));
-    if (hash_table == NULL)
-    {
-        return (NULL);
-    }
+	/* Allocate memory for hash table structure */
+	hash_table = malloc(sizeof(hash_table_t));
+	if (hash_table == NULL)
+	{
+		return (NULL);
+	}
 
-    /* Allocate memory for array of hash nodes */
-    hash_table->array = malloc(sizeof(hash_node_t *) * size);
-    if (hash_table->array == NULL)
-    {
-        free(hash_table);
-        return (NULL);
-    }
+	/* Allocate memory for array of hash nodes */
+	hash_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (hash_table->array == NULL)
+	{
+		free(hash_table);
+		return (NULL);
+	}
 
-    hash_table->size = size;
+	hash_table->size = size;
 
-    /* Initialize all hash node pointers to NULL */
-    for (i = 0; i < size; i++)
-    {
-        hash_table->array[i] = NULL;
-    }
-
-    return (hash_table);
+	/* Initialize all hash node pointers to NULL */
+	for (i = 0; i < size; i++)
+	{
+		hash_table->array[i] = NULL;
+	}
+	return (hash_table);
 }
-

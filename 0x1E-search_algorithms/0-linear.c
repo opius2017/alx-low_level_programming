5b40dabadad489f071fe26b3fcc3bcbde555225d
Owolabi Pius <opius2007@gmail.com>
2023-07-13 07:24:14 +0100

    search algorithms task files

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -22,9 +22,9 @@ int linear_search(int *array, size_t size, int value)
 	{
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
-			{
-				return (i);
-			}
+		{
+			return (i);
+		}
 	}
 	return (-1);
 }

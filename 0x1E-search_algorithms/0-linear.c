c63abf0abdc0d159cb7a00d841374c86119223c2
Owolabi Pius <opius2007@gmail.com>
2023-07-13 10:37:13 +0100

    When you google and press enter

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -10,17 +10,6 @@
  *
  * Return: first index where value is located, or -1 if not found
  */
-#include "search_algos.h"
-
-/**
- * linear_search - searches for a value in an array of
- * integers using the Linear search algorithm
- *
- * @array: input array
- * @size: size of the array
- * @value: value to search in
- * Return: Always EXIT_SUCCESS
- */
 
 int linear_search(int *array, size_t size, int value)
 {
